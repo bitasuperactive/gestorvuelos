7bf560e19c8d252d0a012c8229e712e4641ad422
Vitaliy Balukh <pvitaliy98@hotmail.com>
2024-02-05 03:38:31 +0100

    Documentación finalizada.

---
+++ src/main/java/com/mycompany/gestorvuelos/igu/logica/CompaniaSearchTypeEnum.java
@@ -0,0 +1,26 @@
+package com.mycompany.gestorvuelos.igu.logica;
+
+/**
+ * Valores permitidos en el filtrado de una lista de compañías.
+ */
+public enum CompaniaSearchTypeEnum
+{
+    PREFIJO, 
+    NOMBRE,;
+    
+    /**
+     * Transforma los valores del enumerador en un String Array, permitiéndo su uso en un JComboBox.
+     * @return String Array de los valores del enumerador.
+     */
+    public static String[] valuesToString()
+    {
+        CompaniaSearchTypeEnum[] enumValues = CompaniaSearchTypeEnum.values();
+        String[] enumStringValues = new String[enumValues.length];
+
+        for (int i = 0; i < enumValues.length; i++) {
+            enumStringValues[i] = enumValues[i].toString();
+        }
+        
+        return enumStringValues;
+    }
+}
