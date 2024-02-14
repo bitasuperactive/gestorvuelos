9fa5407298b01093b88cefd9fc1ead0f511600c1
Vitaliy Balukh <pvitaliy98@hotmail.com>
2024-02-14 07:20:53 +0100

    Implementada validación de archivos en vez de rutas (Strings). Clase modularizada y comentada.

--- src/main/java/com/mycompany/gestorvuelos/negocio/logica/PropertiesManager.java
+++ src/main/java/com/mycompany/gestorvuelos/negocio/logica/PropertiesManager.java
@@ -1,6 +1,6 @@
 package com.mycompany.gestorvuelos.negocio.logica;
 
-import com.mycompany.gestorvuelos.dto.CsvPaths;
+import com.mycompany.gestorvuelos.dto.CsvFiles;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,34 +13,102 @@ import java.util.Properties;
  */
 public class PropertiesManager
 {
-    public static CsvPaths getCsvPaths() throws IOException, IllegalArgumentException
-    {
-        try (var csvPathsFile = new FileInputStream(CSVPATHS_PROPERTIES_PATH))
-        {
-            Properties props = new Properties();
-            props.load(csvPathsFile);
-            
-            String aeropuertos, companias, municipios;
-            aeropuertos = props.getProperty("aeropuertos");
-            companias = props.getProperty("companias");
-            municipios = props.getProperty("municipios");
-            
-            System.err.print(aeropuertos);
-            
-            if (aeropuertos == null || aeropuertos.isEmpty() ||
-            companias == null || companias.isEmpty() ||
-            municipios == null || municipios.isEmpty()) 
-            {
-                throw new IllegalArgumentException("Los valores en el archivo {CSVPATHS_PROPERTIES_PATH} están vacíos.");
-            }
-            
-            return new CsvPaths(aeropuertos, companias, municipios);
-        }
-    }
-    
-    // ---> ACCESS MODIFIER: CLASS-PRIVATE <---
+    // <editor-fold defaultstate="collapsed" desc="Propiedades privadas">
     /**
      * Ruta al archivo de propiedades que almacena las rutas de los csvs.
      */
     private static final String CSVPATHS_PROPERTIES_PATH = "src\\main\\java\\com\\mycompany\\gestorvuelos\\CsvPaths.properties";
+    /**
+     * Clave de la propiedad que almacena la ruta al archivo CSV de aeropuertos.
+     */
+    private static final String AEROPUERTOS_PROPERTY = "aeropuertos";
+    /**
+     * Clave de la propiedad que almacena la ruta al archivo CSV de compañías.
+     */
+    private static final String COMPANIAS_PROPERTY = "companias";
+    /**
+     * Clave de la propiedad que almacena la ruta al archivo CSV de municipios.
+     */
+    private static final String MUNICIPIOS_PROPERTY = "municipios";
+    // </editor-fold>
+    
+    /**
+     * Obtiene las representaciones validadas de los archivos CSV requeridos.
+     * @return CsvFiles validado.
+     * @throws IOException Si el archivo de propiedades CsvPaths es innacesible.
+     * @throws IllegalArgumentException Si las rutas son nulas o están vacías.
+     * @throws FileNotFoundException Si los archivos CSV requeridos no existen o no son legibles.
+     * @see CsvFiles
+     */
+    public static CsvFiles getCsvFiles() throws IOException, IllegalArgumentException, FileNotFoundException
+    {
+        // Obtenemos las propiedades de CsvPaths.
+        Properties props = loadProperties();
+
+        // Obtenemos las rutas correspondientes a cada CSV requerido.
+        String aeropuertosCsvPath = props.getProperty(AEROPUERTOS_PROPERTY);
+        String companiasCsvPath = props.getProperty(COMPANIAS_PROPERTY);
+        String municipiosCsvPath = props.getProperty(MUNICIPIOS_PROPERTY);
+
+        // Comprobamos que sean valores válidos.
+        validatePaths(aeropuertosCsvPath, companiasCsvPath, municipiosCsvPath);
+
+        File fileAeropuertos = new File(aeropuertosCsvPath);
+        File fileCompanias = new File(companiasCsvPath);
+        File fileMunicipios = new File(municipiosCsvPath);
+
+        // Comprobamos que las rutas conduzcan a archivos existentes y legibles.
+        checkFileReadability(fileAeropuertos, AEROPUERTOS_PROPERTY);
+        checkFileReadability(fileCompanias, COMPANIAS_PROPERTY);
+        checkFileReadability(fileMunicipios, MUNICIPIOS_PROPERTY);
+
+        return new CsvFiles(fileAeropuertos, fileCompanias, fileMunicipios);
+    }
+
+    /**
+     * Carga el archivo de propiedades CsvPaths.
+     * @return Propiedades CsvPaths.
+     * @throws IOException Si CsvPaths es inaccesible.
+     */
+    private static Properties loadProperties() throws IOException
+    {
+        Properties props = new Properties();
+        try (var csvPathsFile = new FileInputStream(CSVPATHS_PROPERTIES_PATH))
+        {
+            props.load(csvPathsFile);
+        }
+        return props;
+    }
+    
+    /**
+     * Comprueba que la ruta especificada no sea nula o esté vacía.
+     * @param paths Rutas a validar.
+     * @throws IllegalArgumentException Si la ruta especificada es nula o está vacía.
+     */
+    private static void validatePaths(String... paths) throws IllegalArgumentException
+    {
+        for (String path : paths) {
+            if (path == null || path.isEmpty()) {
+                throw new IllegalArgumentException(
+                        "Existen rutas nulas o vacías en el archivo CsvPaths.");
+            }
+        }
+    }
+    
+    /**
+     * Comprueba que el archivo especificado exista y sea legible.
+     * @param file Archivo a validar.
+     * @param propertyName Clave de la propiedad de la que se extrae la ruta al archivo.
+     * @throws FileNotFoundException Si el archivo no existe.
+     * @throws IOException Si el archivo no es legible.
+     */
+    private static void checkFileReadability(File file, String propertyName) throws FileNotFoundException, IOException
+    {
+        if (!file.exists()) {
+            throw new FileNotFoundException(String.format("El archivo CSV \"%s\" no existe.", propertyName));
+        }
+        if (!file.canRead()) {
+            throw new IOException(String.format("El archivo CSV \"%s\" no es legible.", propertyName));
+        }
+    }
 }
