9092113d0be3414586f57ee8df187b267b633ad1
Vitaliy Balukh <pvitaliy98@hotmail.com>
2024-02-16 02:06:13 +0100

    Corregido y adaptado para manejar la habilitación del botón correspondiente

--- src/main/java/com/mycompany/gestorvuelos/igu/listeners/CompaniaValidatorDocumentListener.java
+++ src/main/java/com/mycompany/gestorvuelos/igu/listeners/CompaniaValidatorDocumentListener.java
@@ -1,35 +1,43 @@
-package com.mycompany.gestorvuelos.igu.logica;
+package com.mycompany.gestorvuelos.igu.listeners;
 
 import com.mycompany.gestorvuelos.dto.Compania;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import java.util.Set;
+import javax.swing.JLabel;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
+import com.mycompany.gestorvuelos.igu.interfaces.ValidationFormulary;
 
 /**
  * Validador en tiempo real del input del usuario para los datos de las compañías.
  * <br> (!) Clase descartada al no encontrar una forma eficiente de enviar los mensajes 
  * del validador a la igu.
  */
-@Deprecated
 public class CompaniaValidatorDocumentListener implements DocumentListener
 {
     private Validator validator;
+    private ValidationFormulary parent;
     private String attrName;
+    private JLabel violationLabel;
     private Class<?> fieldType;
 
     /**
      * Valida el atributo vinculado al documento al actualizar su contenido.
+     * @param parent Padre del componente que implimenta este listener.
      * @param attrName Nombre del atributo vinculado al documento que contiene las etiquetas de validación.
+     * @param violationLabel Etiqueta en la que se mostraran los mensajes de error
+     * de validación del atributo.
      * @see Compania
      */
-    public CompaniaValidatorDocumentListener(String attrName)
+    public CompaniaValidatorDocumentListener(ValidationFormulary parent, String attrName, JLabel violationLabel)
     {
         validator = Validation.buildDefaultValidatorFactory().getValidator();
+        this.parent = parent;
         this.attrName = attrName;
+        this.violationLabel = violationLabel;
         
         // Obtenemos el tipo de dato del campo.
         try {
@@ -60,8 +68,8 @@ public class CompaniaValidatorDocumentListener implements DocumentListener
     
     /**
      * Obtiene el objeto representativo del contenido del documento 
-     * dependiendo del tipo de dato del campo (solo wrappers).
-     * <br> Tipos contemplados: String, Short, Integer y Long.
+     * dependiendo del tipo de dato del campo.
+     * <br> Tipos contemplados (solo wrappers): String y Short.
      * @param e Evento del documento.
      * @return Objeto repesentativo del contenido del documento.
      */
@@ -78,10 +86,6 @@ public class CompaniaValidatorDocumentListener implements DocumentListener
                 value = text;
             } else if (fieldType.equals(Short.class)) {
                 value = Short.valueOf(text);
-            } else if (fieldType.equals(Integer.class)) {
-                value = Integer.valueOf(text);
-            } else if (fieldType.equals(Long.class)) {
-                value = Long.valueOf(text);
             }
         } catch (BadLocationException ex) {
             ex.printStackTrace();
@@ -100,46 +104,52 @@ public class CompaniaValidatorDocumentListener implements DocumentListener
     /**
      * Valida el atributo attrName de acuerdo con sus etiquetas de validación.
      * @param value Objeto a validar.
-     * @throws IllegalArgumentException Si el tipo de dato no es compatible para la validación.
+     * @throws IllegalArgumentException Si el tipo del dato no es compatible para la validación.
      * @see jakarta.validation
      */
-    private void validateValue(Object value) throws IllegalArgumentException {        
+    private void validateValue(Object value) throws IllegalArgumentException 
+    {
+        // Comprobamos que el tipo del objeto corresponda con el tipo del campo.
         if (value != null && !value.getClass().equals(fieldType))
         {
-            manageViolation(String.format("El tipo %s no es apto para el atributo %s (%s).",
-                    value.getClass(), attrName, fieldType));
+            manageViolationMessage("Tipo de valor inválido.");
             return;
         }
         
         Set<ConstraintViolation<Compania>> violations;
 
+        // Validamos el objeto en función de su tipo.
         if (value == null) {
             violations = validator.validateValue(Compania.class, attrName, null);
         } else if (value instanceof String) {
-            violations = validator.validateValue(Compania.class, attrName, (String) value);
+            // Se requiere nulificar las cadenas vacías para evitar intromisiones
+            // entre las etiquetas @NotBlank y @Pattern.
+            String str = (String) value;
+            value = (str.isEmpty()) ? null : str;
+            violations = validator.validateValue(Compania.class, attrName, value);
         } else if (value instanceof Short) {
             violations = validator.validateValue(Compania.class, attrName, (Short) value);
-        } else if (value instanceof Integer) {
-            violations = validator.validateValue(Compania.class, attrName, (Integer) value);
-        } else if (value instanceof Long) {
-            violations = validator.validateValue(Compania.class, attrName, (Long) value);
-        }  else {
-            throw new IllegalArgumentException("El tipo de dato no es compatible para la validación.");
+        } else {
+            throw new IllegalArgumentException("El tipo del dato no es compatible para la validación.");
         }
         
-        for (ConstraintViolation<Compania> violation : violations)
-        {
-            manageViolation(violation.getMessage());
+        if (violations.isEmpty()) {
+            manageSuccessValidation();
+            return;
         }
+        
+        manageViolationMessage(violations.iterator().next().getMessage());
     }
     
-    // TODO - Manejar violaciones de validación.
-    /**
-     * Imprime el mensaje correspondiente a la violación de validación.
-     * @param message Mensaje generado por la violación.
-     */
-    private void manageViolation(String message)
+    private void manageSuccessValidation()
     {
-        System.err.println(message);
+        violationLabel.setText("");
+        parent.getSubmitButton().setEnabled(true);
+    }
+    
+    private void manageViolationMessage(String message)
+    {
+        violationLabel.setText(message);
+        parent.getSubmitButton().setEnabled(false);
     }
 }
